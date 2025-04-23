ad3fc370cf3a73d33330f79d16c3478fb3d65083
cauegrazzoliveira <caueoliveiraaa2605@gmail.com>
2025-04-23 16:37:01 -0300

    Delete AED1_2025_1 directory

--- AED1_2025_1/aula02/Cidade.py
+++
@@ -1,7 +0,0 @@
-class Cidade:
-    def __init__(self, name = "Tangamandápio"):
-        self.id = None
-        self.nome = name 
-        
-    def __str__(self):
-        return "Id:" + str(self.id) + " - " + self.nome
