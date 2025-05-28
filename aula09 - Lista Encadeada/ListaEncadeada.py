7abbb50cad97010338741d6ab80730061e719db4
cauegrazzoliveira <caueoliveiraaa2605@gmail.com>
2025-05-28 10:52:54 -0300

    Add files via upload

--- aula09 - Lista Encadeada/ListaEncadeada.py
+++ aula09 - Lista Encadeada/ListaEncadeada.py
@@ -1,6 +1,6 @@
 from No import No
 
-class ListaEcadeada:
+class ListaEncadeada:
     def __init__(self):
         self.inicio = None
 
@@ -11,6 +11,7 @@ class ListaEcadeada:
         else:
             nodo.prox = self.inicio
             self.inicio = nodo
+        self.imprimir()
 
     def addNoFim(self, valor):
         nodo = No(valor)
@@ -24,3 +25,55 @@ class ListaEcadeada:
                 while aux.prox is not None:
                     aux = aux.prox
                 aux.prox = nodo
+        self.imprimir()
+    
+    def imprimir(self):
+        print("---------------------------------")
+        if self.inicio is None:
+            print( "Lista Encadeada está vazia!" )
+        else:
+            aux = self.inicio
+            while aux :
+                print( aux.dado )
+                aux = aux.prox
+
+    def removerDoInicio(self):
+        if self.inicio is not None:
+            self.inicio = self.inicio.prox
+            print("Elemento Removido!")
+
+    def removerDoFim(self):
+        if self.inicio is not None:
+            if self.inicio.prox == None:
+                self.inicio = None
+        else:
+            ant = self.inicio
+            aux = self.inicio.prox
+            while aux.prox:
+                ant = aux
+                aux = aux.prox
+            ant.prox = None
+            print("Elemento Removido!")
+
+    def remover(self, valor):
+        if self.inicio != None:
+            removeu = False
+            if self.inicio.dado == valor:
+                self.inicio = self.inicio.prox
+                removeu = True
+            else:
+                ant = self.inicio
+                aux = self.inicio.prox
+                while aux != None:
+                    if valor == aux.dado:
+                        ant.prox = aux.prox
+                        removeu = True
+                        #break
+                    else:
+                        ant = aux
+                        aux = aux.prox
+                if removeu:
+                    print("Elemento ", valor, " Removido!")
+                else:
+                    print("Elemento Não Encontrado!")
+        self.imprimir()
