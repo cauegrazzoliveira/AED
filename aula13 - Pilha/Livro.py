103b619a07bef593d1ab2ebbb278e837516ac648
cauegrazzoliveira <caueoliveiraaa2605@gmail.com>
2025-07-08 16:44:34 -0300

    Update Livro.py

--- aula13 - Pilha/Livro.py
+++ aula13 - Pilha/Livro.py
@@ -1,6 +1,12 @@
+from Autor import Autor
+
 class Livro:
-    def __init__(self, titulo, autor, paginas):
+    def __init__(self, titulo: str, autor: Autor, paginas: int):
         self.titulo = titulo
-        self.autor = autor
+        self.autor = autor  # Agora é um objeto Autor
         self.paginas = paginas
         self.prox = None
+
+    def __str__(self):
+        # Acessamos o nome do autor através do objeto
+        return f"Título: {self.titulo}, Autor: {self.autor.nome}, Páginas: {self.paginas}"
