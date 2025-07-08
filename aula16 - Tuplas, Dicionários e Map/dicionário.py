d95751c3f33e240f579def70d6a4b8769e2a9c85
cauegrazzoliveira <caueoliveiraaa2605@gmail.com>
2025-07-08 16:27:52 -0300

    Update dicionário.py

--- aula16 - Tuplas, Dicionários e Map/dicionário.py
+++ aula16 - Tuplas, Dicionários e Map/dicionário.py
@@ -11,18 +11,20 @@ carro3 = {
     'ano' : 1986
 }
 
-print(carro1['modelo'])
+print(carro1["modelo"])
 for chave in carro1.keys():
     print(chave)
 
-print("--------------------------------------")
-
-print(carro1['modelo'])
+print("--------------------------------")
 for chave, valor in carro1.items():
-    print(chave, ":", valor)
+    print(chave, ": ", valor)
 
 frota = carro1, carro2
-
-print("--------------------------------------")
-
+print("--------------------------------")
 print(frota)
+print(frota[1])
+#frota[1] = carro3
+carro2['modelo'] = 'Diplomata'
+print(frota)
+
+print("--------------------------------\n\n\n\n\n\n\n\n")
