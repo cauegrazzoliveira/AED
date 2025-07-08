8957a1c8138c590f23e1e8a950167e85f49bd67d
cauegrazzoliveira <caueoliveiraaa2605@gmail.com>
2025-07-08 16:45:16 -0300

    Update main.py

--- aula13 - Pilha/main.py
+++ aula13 - Pilha/main.py
@@ -1,84 +1,68 @@
 from PilhaLivro import PilhaLivro
 from ListaEncadeadaAutor import ListaEncadeadaAutor
 
-def menu_livros(pilha, fila):
+def main():
+    pilha_livros = PilhaLivro()
+    lista_autores = ListaEncadeadaAutor()
+
+    # Dados de exemplo para facilitar o teste
+    lista_autores.adicionar_autor("George Orwell", "Britânico")
+    lista_autores.adicionar_autor("J.R.R. Tolkien", "Britânico")
+    lista_autores.adicionar_autor("Machado de Assis", "Brasileiro")
+
+
     while True:
-        print("\n--- Menu Livros ---")
+        print("\n--- MENU PRINCIPAL ---")
         print("1. Adicionar Livro na Pilha")
         print("2. Remover Livro da Pilha")
         print("3. Imprimir Pilha de Livros")
-        print("4. Voltar ao Menu Principal")
+        print("4. Adicionar Autor")
+        print("5. Imprimir Lista de Autores")
+        print("6. Sair")
         escolha = input("Escolha uma opção: ")
 
-        if escolha == '1':
-            titulo = input("Título do livro: ")
-            autor = input("Autor do livro: ")
-            paginas = int(input("Número de páginas: "))
-            pilha.push(titulo, autor, paginas)
-        elif escolha == '2':
-            pilha.pop()
-        elif escolha == '3':
-            pilha.imprimir()
-        elif escolha == '4':
-            break
-        else:
-            print("Opção inválida!")
+        if escolha == '1': # Adicionar Livro
+            titulo = input("Digite o título do livro: ")
+            nome_autor = input("Digite o nome do autor: ")
 
-def menu_autores(lista_autores):
-    while True:
-        print("\n--- Menu Autores ---")
-        print("1. Adicionar Autor no Início")
-        print("2. Adicionar Autor no Fim")
-        print("3. Remover Autor do Início")
-        print("4. Remover Autor do Fim")
-        print("5. Remover Autor por Nome")
-        print("6. Imprimir Lista de Autores")
-        print("7. Voltar ao Menu Principal")
-        escolha = input("Escolha uma opção: ")
+            autor = lista_autores.buscar_autor(nome_autor)
+            if autor is None:
+                print(f"Autor '{nome_autor}' não encontrado.")
+                add_novo = input("Deseja adicioná-lo agora? (s/n): ").lower()
+                if add_novo == 's':
+                    nacionalidade = input(f"Digite a nacionalidade de {nome_autor}: ")
+                    lista_autores.adicionar_autor(nome_autor, nacionalidade)
+                    autor = lista_autores.buscar_autor(nome_autor) # Busca novamente após adicionar
+                else:
+                    print("Operação cancelada. O livro não foi adicionado.")
+                    continue
 
-        if escolha == '1':
-            nome = input("Nome do autor: ")
-            nacionalidade = input("Nacionalidade do autor: ")
-            lista_autores.addNoInicio(nome, nacionalidade)
-        elif escolha == '2':
-            nome = input("Nome do autor: ")
-            nacionalidade = input("Nacionalidade do autor: ")
-            lista_autores.addNoFim(nome, nacionalidade)
-        elif escolha == '3':
-            lista_autores.removerDoInicio()
-        elif escolha == '4':
-            lista_autores.removerDoFim()
-        elif escolha == '5':
-            nome = input("Nome do autor a ser removido: ")
-            lista_autores.remover(nome)
-        elif escolha == '6':
+            if autor:
+                try:
+                    paginas = int(input("Digite o número de páginas: "))
+                    pilha_livros.push(titulo, autor, paginas)
+                except ValueError:
+                    print("Número de páginas inválido. Operação cancelada.")
+
+        elif escolha == '2': # Remover Livro
+            pilha_livros.pop()
+
+        elif escolha == '3': # Imprimir Pilha
+            pilha_livros.imprimir()
+
+        elif escolha == '4': # Adicionar Autor
+            nome = input("Digite o nome do autor: ")
+            nacionalidade = input("Digite a nacionalidade do autor: ")
+            lista_autores.adicionar_autor(nome, nacionalidade)
+
+        elif escolha == '5': # Imprimir Autores
             lista_autores.imprimir()
-        elif escolha == '7':
-            break
-        else:
-            print("Opção inválida!")
 
-def main():
-    pilha_livros = PilhaLivro()
-    fila_livros = FilaLivro()
-    lista_autores = ListaEncadeadaAutor()
-
-    while True:
-        print("\n--- Menu Principal ---")
-        print("1. Gerenciar Livros")
-        print("2. Gerenciar Autores")
-        print("3. Sair")
-        escolha = input("Escolha uma opção: ")
-
-        if escolha == '1':
-            menu_livros(pilha_livros, fila_livros)
-        elif escolha == '2':
-            menu_autores(lista_autores)
-        elif escolha == '3':
+        elif escolha == '6':
             print("Saindo do programa.")
             break
         else:
-            print("Opção inválida!")
+            print("Opção inválida! Tente novamente.")
 
 if __name__ == "__main__":
     main()
